470edd03f8f56133881c1238ba28dbe86c22026b
pawornwat <ta.pawornwat_st@tni.ac.th>
2022-01-19 12:01:48 +0700

    homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,52 @@
-import React from 'react'
-import { View, Text, StyleSheet ,Button} from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {
+    HeaderButtons,
+    HeaderButton,
+    Item,
+    HiddenItem,
+    OverflowMenu,
+  } from 'react-navigation-header-buttons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const IoniconsHeaderButton = (props) => (
+    // the `props` here come from <Item ... />
+    // you may access them and pass something else to `HeaderButton` if you like
+    <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+  );
+
+
 export default function HomeScreen(navigation) {
-    return (
-        <View style={styles.container}>
-            <Text>
-            หน้าหลัก
-            </Text>
-            <Button
-                title="Go to About"
-                onPress={() => navigation.navigate('About',{email:'aaaa@gmail.com'})}
-            />
-        </View>
-    )
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+        headerLeft:() =>(<HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+            <Item 
+            title="menu" 
+            iconName="menu" 
+            onPress={() => alert('เมนูการทำงาน')} />
+          </HeaderButtons>
+          ),
+    //   headerRight: () => (),
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Ionicons name="home" size={30} color="skyblue"></Ionicons>
+      <Text>หน้าหลัก</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate('About', {email: 'aaaa@gmail.com'})}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
